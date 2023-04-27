976fd91d495a6bfac21d462c692aa27d64570863
David Stanovský <D21940@oaopava.cz>
2023-04-27 15:10:38 +0200

    1st push (cviceni.md & zkouska.py)

---
+++ zkouska.py
@@ -0,0 +1,22 @@
+"""
+#2
+i = 7
+nasobek = 1
+
+while (i * nasobek) <= 70:
+    print(i * nasobek)
+    nasobek +=1
+
+#3
+cislo = int(input("Zadej cislo: "))
+
+for i in range(cislo):
+    print(cislo*i)
+
+"""                                                                     
+#4
+a = []
+
+while len(a) <= 2:
+    a.append(1)
+    print(a)
